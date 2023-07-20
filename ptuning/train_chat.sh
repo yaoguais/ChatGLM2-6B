16b14883c841c6e400405fe4a45eac0e9e9ae6f3
yaoguais <newtopstdio@163.com>
2023-07-20 08:46:00 +0800

    Add test data

--- ptuning/train_chat.sh
+++ ptuning/train_chat.sh
@@ -1,29 +1,28 @@
-PRE_SEQ_LEN=128
-LR=1e-2
-NUM_GPUS=1
+#!/bin/bash
 
-torchrun --standalone --nnodes=1 --nproc-per-node=$NUM_GPUS main.py \
+python main.py \
     --do_train \
-    --train_file $CHAT_TRAIN_DATA \
-    --validation_file $CHAT_VAL_DATA \
+    --train_file minSamples/train.json \
+    --validation_file minSamples/dev.json \
     --preprocessing_num_workers 10 \
     --prompt_column prompt \
     --response_column response \
     --history_column history \
+    --source_prefix "我希望你扮演一个男性虚拟角色西奥多。我会和你进行聊天，请直接回复我，下面是我说的话。[[对话开始]]" \
     --overwrite_cache \
     --model_name_or_path THUDM/chatglm2-6b \
-    --output_dir $CHECKPOINT_NAME \
+    --output_dir output/minsamples-chatglm2-6b-pt-$PRE_SEQ_LEN-$LR \
     --overwrite_output_dir \
-    --max_source_length 256 \
-    --max_target_length 256 \
+    --max_source_length 512 \
+    --max_target_length 512 \
     --per_device_train_batch_size 1 \
     --per_device_eval_batch_size 1 \
     --gradient_accumulation_steps 16 \
     --predict_with_generate \
-    --max_steps 3000 \
+    --max_steps 100 \
     --logging_steps 10 \
-    --save_steps 1000 \
-    --learning_rate $LR \
-    --pre_seq_len $PRE_SEQ_LEN \
+    --save_steps 100 \
+    --learning_rate 2e-2 \
+    --pre_seq_len 128 \
     --quantization_bit 4
 
